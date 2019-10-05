269af83f96f51d2a130d49ef99e905648670503b
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2019-10-05 23:57:35 +0300

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Yurii Serediuk on 10/5/19.
+//  Copyright © 2019 Yurii Serediuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
